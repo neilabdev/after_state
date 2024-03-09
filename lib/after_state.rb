e917f10f28bda9526c2e62444486f6110000609f
James Whitfield <2140679+neilabdev@users.noreply.github.com>
2024-03-09 12:27:10 -0500

    Fixed issue of AfterState::Change not being included

--- lib/after_state.rb
+++ lib/after_state.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "after_state/version"
+require_relative "after_state/changed"
 
 module AfterState
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require 'after_state/railtie'  if defined?(Rails::Railtie)
